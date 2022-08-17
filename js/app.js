73543b51ef0ae008789a1ff93820d6f81235d373
GiORDiA <giordiariel.fullstackdev@gmail.com>
2022-08-17 09:45:58 -0300

    Construcción de Secciones del template y finalizando con Footer

--- js/app.js
+++ js/app.js
@@ -7,6 +7,13 @@ function toggleMenu() {
   menu.classList.toggle("menu_opened"); // función para que el menu, si esta abierto lo cierre y viceversa.
 }
 
- openMenuBtn.addEventListener("click", toggleMenu) // asignamos el evento "click" a la classe abrir cuando escuche el clickeo.
- closeMenuBtn.addEventListener("click", toggleMenu) // asignamos el evento "click" a la classe cerrar cuando escuche el clickeo.
- 
+ openMenuBtn.addEventListener("click", toggleMenu); // asignamos el evento "click" a la classe abrir cuando escuche el clickeo.
+ closeMenuBtn.addEventListener("click", toggleMenu); // asignamos el evento "click" a la classe cerrar cuando escuche el clickeo.
+
+const menuLinks = document.querySelectorAll('.menu a[href^="#"]'); // selecciona todo los atributos que tengan href que comience con #, y vamos a:
+// recorrer todos los enlaces y por cada uno de esos enlaces vamos añadir un EventListener que escuchara "click" y ejecutara una función que eliminara el menu_opened.
+menuLinks.forEach(menuLink => {
+  menuLink.addEventListener("click", function() {
+    menu.classList.remove("menu_opened");
+  })
+})
